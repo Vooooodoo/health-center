cb78179e5ad6be2902fdae49dbedf133a95e38f5
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-10-07 15:45:57 +0300

    feat: реализовать мобильное меню

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,9 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const mobileMenuButton = document.querySelector('.header__button');
 
 // FUNCTIONS
-function toggleMobileMenu() {
+function switchMobileMenu() {
   mobileMenu.classList.toggle('mobile-menu_opened');
 }
 
 // LISTENERS
-mobileMenuButton.addEventListener('click', toggleMobileMenu);
+mobileMenuButton.addEventListener('click', switchMobileMenu);
