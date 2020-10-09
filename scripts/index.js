c581d2d1e2fdad085b25e267f90d801cc5c7a44e
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-10-09 16:35:22 +0300

    feat: реализовать попапы

--- scripts/index.js
+++ scripts/index.js
@@ -7,5 +7,30 @@ function switchMobileMenu() {
   mobileMenu.classList.toggle('mobile-menu_opened');
 }
 
+function openPopup(evt) {
+  const clickedCard = evt.target.parentElement;
+  const clickedCardPopup = clickedCard.lastElementChild;
+
+  if (evt.target.classList.contains('procedures__card-photo') || evt.target.classList.contains('procedures__card-title')) {
+    clickedCardPopup.classList.add('popup_opened');
+  }
+}
+
+function closePopup(evt) {
+  if (evt.target.classList.contains('popup')) {
+    evt.target.classList.remove('popup_opened');
+  }
+
+  if (evt.target.classList.contains('popup__close')) {
+    const popupContainer = evt.target.parentElement;
+    const clickedCardPopup = popupContainer.parentElement;
+
+    clickedCardPopup.classList.remove('popup_opened');
+  }
+}
+
 // LISTENERS
 mobileMenuButton.addEventListener('click', switchMobileMenu);
+
+document.addEventListener('click', openPopup);
+document.addEventListener('click', closePopup);
