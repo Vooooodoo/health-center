2d5ed33b7d4267c8e5843bb79b284e7cfd59ae3a
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-12-04 20:29:29 +0300

    feat: реализовать попапы на странице регистрации

--- scripts/registration.js
+++ scripts/registration.js
@@ -1,32 +1,52 @@
 // DOM-ELEMENTS
 const mobileMenu = document.querySelector('.mobile-menu');
 const mobileMenuButton = document.querySelector('.header__button');
-const photoPopup = document.querySelector('#photo-popup');
-const photoPopupImg = photoPopup.querySelector('.popup__photo');
+const scheduleBtn = document.querySelector('#schedule-btn');
+const priceBtn = document.querySelector('#price-btn');
+const schedule2020Popup = document.querySelector('#schedule2020-popup');
+const schedule2021Popup = document.querySelector('#schedule2021-popup');
 
 // FUNCTIONS
 function switchMobileMenu() {
   mobileMenu.classList.toggle('mobile-menu_opened');
 }
 
-function openPhotoPopup(evt) {
-  if (evt.target.classList.contains('documents__photo')) {
-    photoPopup.classList.add('popup_opened');
-    photoPopupImg.src = evt.target.src;
-  }
+function openSchedule2020Popup() {
+  schedule2020Popup.classList.add('popup_opened');
 }
 
-function closePhotoPopup(evt) {
+function closeSchedulePopup(evt) {
   const isPopup = evt.target.classList.contains('popup');
   const isPopupClose = evt.target.classList.contains('popup__close');
 
   if (isPopup || isPopupClose) {
-    photoPopup.classList.remove('popup_opened');
+    schedule2020Popup.classList.remove('popup_opened');
+    schedule2021Popup.classList.remove('popup_opened');
+  }
+}
+
+function closeSchedule2020Popup(evt) {
+  const isPopupSliderBtn = evt.target.classList.contains('popup__slider-btn_right');
+
+  if (isPopupSliderBtn) {
+    schedule2020Popup.classList.remove('popup_opened');
+    schedule2021Popup.classList.add('popup_opened');
+  }
+}
+
+function closeSchedule2021Popup(evt) {
+  const isPopupSliderBtn = evt.target.classList.contains('popup__slider-btn_left');
+
+  if (isPopupSliderBtn) {
+    schedule2021Popup.classList.remove('popup_opened');
+    schedule2020Popup.classList.add('popup_opened');
   }
 }
 
 // LISTENERS
 mobileMenuButton.addEventListener('click', switchMobileMenu);
 
-document.addEventListener('click', openPhotoPopup);
-document.addEventListener('click', closePhotoPopup);
+scheduleBtn.addEventListener('click', openSchedule2020Popup);
+document.addEventListener('click', closeSchedule2020Popup);
+document.addEventListener('click', closeSchedule2021Popup);
+document.addEventListener('click', closeSchedulePopup);
